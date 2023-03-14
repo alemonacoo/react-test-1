1a05ebe0ecf113628ad3372ff0a836720cdd3a60
alemonacoo <ale.monaco@outlook.com>
2023-03-14 12:06:09 +0100

    changing app className

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header";
+
 const App = () => {
   const x = true;
 
   return (
-    <div className="App">
+    <div className="container">
+      <Header title="Hello From HEADER" />
+      {/* <Header />  HEADER DI DEFAULT SENZA PROPS*/}
       <h1>Hello From React!</h1>
       <h2>Hello {x ? "John" : "Mike"}</h2>
     </div>
