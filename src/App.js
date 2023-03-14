d0ccbfa5a30f5839a4154a220ea814943b5dba68
alemonacoo <ale.monaco@outlook.com>
2023-03-14 10:02:47 +0100

    cleaning up startup files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const x = true;
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello From React!</h1>
+      <h2>Hello {x ? "John" : "Mike"}</h2>
     </div>
   );
-}
+};
 
 export default App;
